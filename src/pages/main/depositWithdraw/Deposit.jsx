366efb9c8106355015f506625315e4403f8b9dac
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-09-08 18:13:25 +0430

    fixed bug

--- src/pages/main/depositWithdraw/Deposit.jsx
+++ src/pages/main/depositWithdraw/Deposit.jsx
@@ -69,40 +69,18 @@ const Deposit = () => {
                     isDisabled={!isActive}
                   />
                 </div>
-                <div className="flex gap-10 lg:flex  md:block justify-center  ml-5 mr-5 mt-1">
-                  <label htmlFor="" className="w-30 mt-1 ">
+                <div className="flex gap-10 lg:flex  md:block justify-center ml-5 mr-5 mt-1">
+                  <label htmlFor="" className="w-30 mt-1">
                     {t('Amount')}:
                   </label>
-                  <div className="flex items-center max-w-60  rounded-md bg-white pl-2 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-                    <div className="shrink-0 text-base pl-1 rtl:mr-5 text-gray-500 select-none sm:text-sm/6">
-                      $
-                    </div>
-                    <input
-                      id="price"
-                      name="price"
-                      type="text"
-                      placeholder="0.00"
-                      className="block min-w-0 grow py-1.5 pr-2 w-48 ltr:w-53 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                    />
-                    <div className="grid shrink-0 grid-cols-1 focus-within:relative">
-                      <select
-                        id="currency"
-                        name="currency"
-                        aria-label="Currency"
-                        className="col-start-1 row-start-1 ltr:mr-3  appearance-none rounded-md py-1.5 pr-3 ml-2 pl-2 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                      >
-                        <option>USD</option>
-                        <option>CAD</option>
-                        <option>EUR</option>
-                      </select>
-                      <BiChevronDown
-                        aria-hidden="true"
-                        className="pointer-events-none col-start-1 row-start-1 mr-1 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                      />
-                    </div>
-                  </div>
+                  <input
+                    type="text"
+                    id="number"
+                    aria-describedby="helper-text-explanation"
+                    class="border border-gray-300 shadow-cyan-400 max-w-58 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 focus:outline-indigo-600"
+                    required
+                  />
                 </div>
-
                 <div className="flex gap-10 lg:flex  md:block justify-center ml-5 mr-5 mt-1">
                   <label htmlFor="" className="w-30 mt-1">
                     {t('Date')}:
