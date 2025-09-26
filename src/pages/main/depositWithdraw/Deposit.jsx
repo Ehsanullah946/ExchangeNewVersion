4916ff589dd90b52215a76f14cf43cd82fd7d829
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-09-26 17:41:23 +0430

    deposit completed

--- src/pages/main/depositWithdraw/Deposit.jsx
+++ src/pages/main/depositWithdraw/Deposit.jsx
@@ -5,10 +5,84 @@ import { BsListCheck, BsPrinter, BsSearch } from 'react-icons/bs';
 
 import { useTranslation } from 'react-i18next';
 import { FaRegArrowAltCircleDown } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useToast } from '../../../hooks/useToast';
+import { useCreateDeposit } from '../../../hooks/useDeposit';
+import { useAccount } from '../../../hooks/useAccount';
 const Deposit = () => {
   const [isActive, setIsActive] = useState(false);
   const { t } = useTranslation();
+
+  const toast = useToast();
+  const { mutate, isLoading } = useCreateDeposit();
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    deposit: '',
+    withdraw: '',
+    description: '',
+    accountNo: '',
+    employeeId: '',
+    DWData: '',
+  });
+
+  const { data: accountResponse } = useAccount();
+
+  const accountOptions = (accountResponse?.data || []).map((c) => ({
+    value: c.No,
+    label: `${c.Customer?.Stakeholder?.Person?.firstName} - ${c.MoneyType.typeName}`,
+  }));
+
+  const handleChange = (e) => {
+    const { value, name, checked, type } = e.target;
+    setForm((prev) => ({
+      ...prev,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!form.deposit || !form.accountNo) {
+      toast.error(t('Please Enter deposit and account'));
+      return;
+    }
+
+    const payload = {
+      ...form,
+      deposit: parseFloat(form.deposit) || 0,
+    };
+
+    const cleanData = Object.fromEntries(
+      Object.entries(payload).filter(
+        ([_, v]) => v !== '' && v !== null && v !== undefined
+      )
+    );
+    mutate(cleanData, {
+      onSuccess: () => {
+        toast.success(t('Deposit Created'));
+        navigate('/main/depositList');
+      },
+      onError: (error) => {
+        console.error('Backend error:', error);
+        let errorMessage = t('createDepositFailed');
+
+        if (error.response?.data?.message) {
+          errorMessage = error.response.data.message;
+
+          if (error.response.data.message.includes('Validation error')) {
+            errorMessage = t('DepositDuplicate');
+          } else if (error.response.data.message.includes('validation')) {
+            errorMessage = t('invalidInputData');
+          }
+        }
+
+        toast.error(errorMessage);
+      },
+    });
+  };
+
   return (
     <>
       <div className="grid justify-center">
@@ -48,41 +122,45 @@ const Deposit = () => {
                 {t('Deposit')} <FaRegArrowAltCircleDown className="mt-1" />
               </span>
             </div>
-
             <div className="grid sm:grid-cols-2 gap-6 p-3 rounded-b-2xl ltr:mr-4 rtl:ml-4 px-4 md:px-6 lg:px-10 border-b-2 border-t-2 shadow-2xl w-full max-w-7xl mx-auto">
               <div className=" space-y-1 w-full">
                 <div className="flex gap-6 flex-wrap md:flex-nowrap justify-between ">
-                  <label className="sm:w-32">{t('Account No')}:</label>
-                  <input
-                    type="text"
-                    className="border border-gray-300 shadow-sm text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1"
-                    required
-                  />
-                </div>
-
-                <div className="flex gap-6 flex-wrap md:flex-nowrap justify-between ">
                   <label className="sm:w-32">{t('Account')}:</label>
                   <Select
-                    className="w-full shadow-sm "
-                    name="branch"
+                    className="w-full shadow-sm"
+                    name="accountNo"
                     isSearchable
-                    isDisabled={!isActive}
+                    options={accountOptions}
+                    value={accountOptions.find(
+                      (opt) => opt.value === form.accountNo
+                    )}
+                    onChange={(selected) =>
+                      setForm((prev) => ({
+                        ...prev,
+                        accountNo: selected?.value || '',
+                      }))
+                    }
                   />
                 </div>
-
                 <div className="flex gap-6 flex-wrap md:flex-nowrap justify-between ">
                   <label className="sm:w-32">{t('Amount')}:</label>
                   <input
-                    type="text"
-                    className=" w-full border border-gray-300 shadow-sm text-red-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1"
+                    type="number"
+                    name="deposit"
+                    onChange={handleChange}
+                    value={form.deposit}
+                    className=" w-full border border-gray-300 shadow-sm  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1"
                     required
                   />
                 </div>
-                <div className="flex gap-6 flex-wrap md:flex-nowrap justify-between ">
-                  <label className="sm:w-32">{t('Date')}:</label>
+                <div className="flex gap-6 justify-between">
+                  <label className="sm:w-32 mt-2">{t('Date')}:</label>
                   <input
                     type="date"
-                    className="w-full border border-gray-300 shadow-sm text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1"
+                    name="DWData"
+                    value={form.DWData}
+                    onChange={handleChange}
+                    className="border shadow-sm rounded-lg w-full p-1"
                     required
                   />
                 </div>
@@ -91,28 +169,31 @@ const Deposit = () => {
                   <label className="sm:w-32 mt-1">{t('Description')}:</label>
                   <textarea
                     rows="4"
+                    name="description"
+                    onChange={handleChange}
+                    value={form.description}
                     className="w-full border border-gray-300 shadow-sm text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1"
                     placeholder="بشتر ..........."
                   />
                 </div>
 
-                <div className="flex flex-wrap gap-3 mt-3 justify-center sm:justify-start">
-                  {isActive ? (
-                    <>
-                      <Button type="primary" htmlType="submit">
-                        {t('Save')}
-                      </Button>
-                      <Button type="primary">{t('Cancel')}</Button>
-                    </>
-                  ) : (
-                    <>
-                      <Button type="primary" onClick={() => setIsActive(true)}>
-                        {t('New')}
-                      </Button>
-                      <Button type="primary">{t('Edit')}</Button>
-                      <Button type="primary">{t('Delete')}</Button>
-                    </>
-                  )}
+                <div className="flex flex-wrap justify-center sm:justify-start gap-2 col-span-full">
+                  <button
+                    onClick={handleSubmit}
+                    disabled={isLoading}
+                    type="button"
+                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-4 py-1 text-center me-2 mb-2 "
+                  >
+                    {t('Save')}
+                  </button>
+                  <Link to="/main/depositList">
+                    <button
+                      type="button"
+                      classname="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-4 py-1 text-center me-2 mb-2"
+                    >
+                      {t('Cancel')}
+                    </button>
+                  </Link>
                 </div>
               </div>
 
